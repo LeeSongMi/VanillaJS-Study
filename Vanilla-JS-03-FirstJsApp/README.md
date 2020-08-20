3ed10315f4f63d117ac51c3fd1955ec5e5ecc63d
LeeSongMi <lssong9631@gmail.com>
2020-08-20 13:51:08 +0900

    EndVanillaJS

--- Vanilla-JS-03-FirstJsApp/README.md
+++ Vanilla-JS-03-FirstJsApp/README.md
@@ -12,11 +12,15 @@
 - 3.11 Conclusions
 
 ## 3.1-2 Making a JS Clock Part
+: 시계 앱 제작
+- clock.js
 - setInterval(func,time) : 해당 함수를 지정 시간마다 초기화
 - ` 문자 ${변수} ` : 문자와 변수를 함꼐 string type으로 작성
 <br>
 
 ## 3.3-4 Saving the User Name Part
+: 유저 이름 입력 및 로컬 스토리지에 저장, 이름 출력
+- gretting.js
 - localStorage: 소규모 데이터 저장, String파일만 읽음
 - .setItem/.getItem : 저장/불러오기
 - classList: JS에서 클래스 이름 조작
@@ -24,6 +28,8 @@
 <br>
 
 ## 3.5-7 Making a To Do List Part
+: ul을 이용한 todo리스트 제작 및 요소 추가 삭제
+- todo.js
 - parseInt/Float : 데이터 타입을 int, float 형태로 변환
 - JSON: JS가 데이터를 다룰 수 있도록 타입 변환 (ex: string -> object)
 - list.filter(func): 리스트 내의 모든 값에 해당 함수 적용
@@ -32,10 +38,19 @@
 <br>
 
 ## 3.8 Image Background
+: 배경이미지 설정
+- bg.js, index.css
 - Math.floor/.ceiling: 소숫점 버림/올림
 - Math.random() * Number: 랜덤 숫자 생성
 <br>
 
-## 3.9 Getting The Weather Part One(Geolocation)
-## 3.10 Getting The Weather Part Two(API)
+## 3.9-10 Getting The Weather Part One(Geolocation,API)
+: 위치 정보 저장 및 불러오기, 위도, 경도 찾기, API를 이용하여 데이터 불러오기
+- navigator API
+- navigator.geolocation.getCurrentPosition(정상,오류): 위치 획득
+- fetch(url,[options]): 네트워크 요청
+- .then(): 앞의 작업이 완료될때까지 기다린 후 실행
+<br>
+ 
+ 
 ## 3.11 Conclusions
--- Vanilla-JS-03-FirstJsApp/weather.js
+++ Vanilla-JS-03-FirstJsApp/weather.js
@@ -1,7 +1,34 @@
+const weather = document.querySelector(".js-weather");
+const API_KEY ="9175de3a26f46233397ea9877ff6b1ab";
 const COORDS = 'coords';
 
-function handleGeoSource(position){
-    console.log(position);
+function getWeather(lat,lon){
+    //fetch(url,[options]) :네트워크 요청
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    ).then(function(response){      // fetch가 완료될 때까지 기다려야하기 때문에 then 사용
+        //console.log(response.json());
+        return response.json();
+    }).then(function(json){
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+
+}
+function saveCoords(coordsObj){
+    // object를 string으로 변환하여 localStorage에 저장
+    localStorage.setItem(COORDS,JSON.stringify(coordsObj))
+}
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;  //위도
+    const longitude = position.coords.longitude;    //경도
+    const coordsObj = {
+        latitude,       // === (latitude: latitude):변수명과 키가 같을 때 사용 
+        longitude
+    }
+    saveCoords(coordsObj);
+    getWeather(latitude,longitude);
 }
 function handleGeoError(){
     console.log("ERROR");
@@ -9,13 +36,15 @@ function handleGeoError(){
 
 function askForCoords(){
     //navigator API
-    navigator.geolocation.getCurrentPosition(handleGeoSource,handleGeoError); //위치 획득
+    navigator.geolocation.getCurrentPosition(handleGeoSucces,handleGeoError); //위치 획득
 }
 function loadCoords(){
     const loadedCords = localStorage.getItem(COORDS);
     if(loadedCords === null){
         askForCoords();
     }else{
+        const parseCoords = JSON.parse(loadedCords);
+        getWeather(parseCoords.latitude,parseCoords.longitude);
 
     }
 }
